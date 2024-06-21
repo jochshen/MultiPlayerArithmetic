dc7e2443027e369f06d502ad3c306bc2a797ac32
jochshen <joshchen43@gmail.com>
2024-06-21 16:05:53 -0700

    changed router to github supported version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import StartingScreen from "./components/StartingScreen";
 import PlayingScreen from "./components/PlayingScreen";
 
